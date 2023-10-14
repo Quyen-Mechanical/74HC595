c7b8c69c53236541718f0fad79880e1a80d51fb2
Quyen-Mechanical <uchihayrioku009@gmail.com>
2023-10-14 20:04:41 +0700

    new file:   Project Backups/Arduino 328 [Autosaved].pdsprj 	new file:   proteus/Arduino 328.pdsprj 	new file:   proteus/Arduino 328.pdsprj.THEARTALT.quyen.workspace 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,44 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void send(uint8_t data)
+{
+  digitalWrite(10, 0);
+  shiftOut(11, 13, LSBFIRST, data);
+  digitalWrite(10, 1);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  pinMode(10, OUTPUT); // LATCH SLAVE SELECT
+  pinMode(11, OUTPUT); // DATA SERIAL (DATAPIN)
+  pinMode(13, OUTPUT); // CLOCK PIN SCK
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+
+  // digitalWrite(10, 0);
+  // shiftOut(11, 13, LSBFIRST, 0b01010101);
+  // digitalWrite(10, 1);
+  // delay(300);
+  // digitalWrite(10, 0);
+  // shiftOut(11, 13, LSBFIRST, 0b10101010);
+  // digitalWrite(10, 1);
+  // delay(300);
+   // digitalWrite(10, 0);
+  // shiftOut(11, 13, LSBFIRST, 0b11111111 );
+  // digitalWrite(10, 1);
+  // delay(500);
+   // digitalWrite(10, 0);
+  // shiftOut(11, 13, LSBFIRST,0b00000000 );
+  // digitalWrite(10, 1);
+  // delay(1000);
+  send(0b01010101);
+  delay(300);
+  send(0b10101010);
+  delay(300);
+  send(0b11111111);
+  delay(500);
+  send(0b00000000);
+  delay(1000);
 }
