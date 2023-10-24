77a28a07adf12b9a599dfb0047913ad8e7a734d9
Quyen-Mechanical <uchihayrioku009@gmail.com>
2023-10-24 21:46:51 +0700

    modified:   Project Backups/Arduino 328 [Autosaved].pdsprj 	modified:   src/module/hc595.cpp 	modified:   src/module/hc595.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,22 @@
-#include<Arduino.h>
-#include<module/hc595.h>
-HC595 ic1(13,11,10);
+#include <Arduino.h>
+#include <module/hc595.h>
+HC595 ic1(10, 13, 11);
 
-void setup(){
-  ic1.bat_chan(3);
+void setup()
+{
+  Serial.begin(9600);
+  Serial.print("chan clock cau ic 1 la :");
+  Serial.println(ic1.clock);
+  uint8_t chan_can_dieu_khien = 9;
+  bool thanh_cong = ic1.bat_chan(chan_can_dieu_khien);
+  if (thanh_cong){
+    delay(500);
+    ic1.tat_chan(chan_can_dieu_khien);
+  }else{
+      Serial.println("loi");
+  }
 }
 
-void loop(){
-    
+void loop()
+{
 }
--- src/module/hc595.h
+++ src/module/hc595.h
@@ -3,13 +3,17 @@
 class HC595
 {
 private:
+
+
+public:
     uint8_t latch;
     uint8_t data;
     uint8_t clock;
     uint8_t port;
-
-public:
     HC595(uint8_t chan_chot, uint8_t chan_xung_thoi_gian, uint8_t chan_tin_hieu);
     ~HC595();
-    void bat_chan(uint8_t chan_can_bat);
+    bool bat_chan(uint8_t chan_can_bat1);
+    void tat_chan(uint8_t chan_can_tat);
+    void send(uint8_t register_to_port);
+
 };
